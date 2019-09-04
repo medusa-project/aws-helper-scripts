09114c18a31bcc0ef70c21bc2de1dbf9635d04a5
Troy, Joseph M <jmtroy2@illinois.edu>
2019-09-04 15:44:41 -0500

    add list-snapshots.sh script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aws-helper-scripts
 Various scripts to help with AWS tasks
+
+list-snapshots.sh - a script that uses aws cli to list snapshots for a EBS Volume given the Name tag value.
---
+++ list-snapshots.sh
@@ -0,0 +1,13 @@
+#! /bin/bash 
+
+echo
+echo "This script lists snapshots for the EBS Volume with the 'Name' tag that you enter below (medusa-prod, databank-demo,etc.)."
+echo "NOTE: Volume 'Name' tags are the same as the EC2 'Name' tag."
+echo
+echo "Enter Volume 'Name' tag value: \c"
+
+read NameTAG
+
+echo "SnapShotID--------------Progress--Size-----SnapShotStartDate------------VolumeId"
+
+aws ec2 describe-snapshots --filters Name=tag:Name,Values="$NameTAG" --query "Snapshots[*].{Time:StartTime,ID:SnapshotId,Volume:VolumeId,SizeinGB:VolumeSize,Progress:Progress}"  --output text | sort -k4
